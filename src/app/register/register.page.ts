fcd13be6b4a49aab63d666ea1838cb3ed9c29cbe
juliscotto <juliscotto@gmail.com>
2019-08-04 22:28:19 -0300

    test

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { auth } from 'firebase/app'
 import { AngularFireAuth } from '@angular/fire/auth'
 
+import { AlertController } from '@ionic/angular'
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -14,24 +16,40 @@ export class RegisterPage implements OnInit {
 	cpassword: string = ""
 
 
-	constructor(public afAuth: AngularFireAuth) { }
+	constructor(
+		public afAuth: AngularFireAuth,
+		public alert: AlertController		
+		) { }
 
 	ngOnInit() {
 	}
 
 	async register(){
 		const { email, password, cpassword } = this
-		if(password != cpassword) {
+		if(password !== cpassword) {
+			this.showAlert("Error", "Passwords don't match")
 			return console.error("Passwords don't match!")
 		}
 
 		try{
 			const res = await this.afAuth.auth.createUserWithEmailAndPassword(email, password)
 			console.log(res)
+			this.showAlert("Success!", "Walcome")
 		} catch(error){
 			console.dir(error)
+			this.showAlert("Error", error.message)
 		}
 		
+	}
+
+	async showAlert(header: string, message: string){
+		const alert = await this.alert.create({
+			header,
+			message,
+			buttons: ["Ok"]
+		})
+
+		await alert.present()
 	}
 
 }
